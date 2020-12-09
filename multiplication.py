076147a720effc8388e3352fb7d025b63a50b2a5
mbs1989 <myriambensliman@gmail.com>
2020-12-09 21:59:38 +0100

    Fonction multiplication.py

---
+++ multiplication.py
@@ -0,0 +1,16 @@
+
+
+
+def multiplication(x,y):
+    resultat = x*y
+    return resultat
+
+mon_premier_chiffre = int(input("donner le premier chiffre "))
+mon_deuxieme_chiffre = int(input("donner le deuxieme chiffre "))
+
+print("mon premier chiffre est ", mon_premier_chiffre)
+print("mon deuxieme chiffre est ", mon_deuxieme_chiffre)
+
+resultat = multiplication(mon_premier_chiffre, mon_deuxieme_chiffre)
+
+print("mon resultat est ", resultat)
