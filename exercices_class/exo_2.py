1101b457c196cac5ec94bdac3adcc869145bacd4
mbs1989 <myriambensliman@gmail.com>
2021-05-05 17:55:50 +0200

    exo_2.py

--- exercices_class/exo_2.py
+++ exercices_class/exo_2.py
@@ -7,3 +7,24 @@ of Bus.seating_capacity() a default value of 50.
 Use the following code for your parent Vehicle class. You need to use method overriding.
 Define property that should have the same value for every class instance
 """
+
+class Vehicle:
+    color = "White"
+    def __init__(self, max, mile, name):
+        self.max_speed = max
+        self.mileage = mile
+        self.name = name
+
+    def seating_capacity(self, capacity):
+        self.capacity = capacity
+        return f"the seating capacity of a {self.name} is {self.capacity} passengers"
+
+class Bus(Vehicle):
+    def seating_capacity(self, capacity=50):
+        return super().seating_capacity(capacity=50)
+
+volvo = Vehicle(200,150, "Volvo")
+print(volvo.max_speed)
+school_bus = Bus(180,12,"School Volvo")
+print(school_bus.max_speed, school_bus.mileage, school_bus.name)
+print(school_bus.seating_capacity())
